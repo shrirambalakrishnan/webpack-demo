9b13207052fd66811b55ae9d65a11b130d4c77eb
shriram <shriram.balakrishnan@in.reportbee.com>
2019-05-18 21:14:10 +0530

    Handle images using file-loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from "lodash";
 import "./style.css";
+import spiderManIcon from "./spider_man.jpg";
 
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello World", "From Webpack"], " ");
   element.classList.add("panel");
+
+  const icon = new Image();
+  icon.src = spiderManIcon;
+  element.appendChild(icon);
 
   return element;
 }
