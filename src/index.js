aee65c1d54885c3353a25dd46b9c2b24d969bad4
shriram <shriram.balakrishnan@in.reportbee.com>
2019-05-18 19:14:42 +0530

    Configure webpack to enable importing CSS files into JS
    1. Used css-loader and style-loader packages for this.
    2. Configure webpack to use the above loaders for CSS files using "rules" in webpack config.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
 
-function component(){
-  const element = document.createElement('div');
-  element.innerHTML = _.join(["Hello World", "From Webpack"], ' ');
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["Hello World", "From Webpack"], " ");
+  element.classList.add("panel");
 
   return element;
 }
