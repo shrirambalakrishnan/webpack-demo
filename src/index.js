d9f5820ce5ece013846c73001ea8cb02dbaf3074
shriram <shriram.balakrishnan@in.reportbee.com>
2019-05-18 18:27:45 +0530

    Render page by loading a js package using script tag
    That is, inclusion of JS Library (Loadash) happens at one part of code and the usage of that library happens at other part of code (in the JS code).

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function component(){
+  const element = document.createElement('div');
+  element.innerHTML = _.join(["Hello World", "From Webpack"], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
